f58fd7482418ca0daa75a84a5d3a6369602ba27c
n0k1b <nokibevon7@gmail.com>
2021-07-26 02:10:53 +0600

    handler exception open

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -57,10 +57,10 @@ class Handler extends ExceptionHandler
 
     public function render($request, Throwable $exception)
     {
-        // if ($exception instanceof NotFoundHttpException || $exception instanceof MethodNotAllowedHttpException || $exception instanceof ModelNotFoundException || $exception instanceof InvalidArgumentException || $exception instanceof ErrorException || $exception instanceof QueryException || $exception instanceof Error ||$exception instanceof BadMethodCallException ) {
+        if ($exception instanceof NotFoundHttpException || $exception instanceof MethodNotAllowedHttpException || $exception instanceof ModelNotFoundException || $exception instanceof InvalidArgumentException || $exception instanceof ErrorException || $exception instanceof QueryException || $exception instanceof Error ||$exception instanceof BadMethodCallException ) {
 
-        //     return response()->view('error', [], 500);
-        // }
+            return response()->view('error', [], 500);
+        }
         return parent::render($request, $exception);
     }
 
