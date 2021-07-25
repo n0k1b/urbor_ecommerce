1db8748288418792d3d7822a08be3317479f63d1
n0k1b <nokibevon7@gmail.com>
2021-07-25 21:37:05 +0600

    carousel add to index

--- resources/views/admin/category/edit_category_image.blade.php
+++ resources/views/admin/category/edit_category_image.blade.php
@@ -8,14 +8,14 @@
 				<div class="row page-titles mx-0">
                     <div class="col-sm-6 p-md-0">
                         <div class="welcome-text">
-                            <h4>Add Content</h4>
+                            <h4>Edit Category</h4>
                         </div>
                     </div>
                     <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                            <li class="breadcrumb-item active"><a href="javascript:void(0);">category Content</a></li>
-                            <li class="breadcrumb-item active"><a href="javascript:void(0);">Add Content</a></li>
+                            <li class="breadcrumb-item"><a href="{{ url('admin') }}">Home</a></li>
+                            <li class="breadcrumb-item active"><a href="javascript:void(0);">Category</a></li>
+                            <li class="breadcrumb-item active"><a href="javascript:void(0);">Edit</a></li>
                         </ol>
                     </div>
                 </div>
@@ -24,7 +24,7 @@
 					<div class="col-xl-12 col-xxl-12 col-sm-12">
                         <div class="card">
                             <div class="card-header">
-								<h5 class="card-title">Content Image</h5>
+								<h5 class="card-title">Category Image</h5>
 							</div>
 							<div class="card-body">
                                 <form action="{{route('update_category_image')}}" method="post" enctype="multipart/form-data">
@@ -36,7 +36,7 @@
 										<div class="col-lg-12 col-md-12 col-sm-12">
 											<div class="form-group fallback w-100">
                                                 <div class="field" align="left">
-													<label class="form-label">Domain Image</label>
+
                                                     <input type="file" id="files" name="image" />
                                                     <input name="id" type="hidden" value="{{$data->id}}">
 												  </div>
