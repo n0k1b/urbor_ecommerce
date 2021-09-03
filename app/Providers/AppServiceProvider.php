55aececf3bba51034d81f730f666a7bed2cd5eb2
n0k1b <nokibevon7@gmail.com>
2021-09-03 20:42:26 +0600

    force https added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //test
 
-      //  \Illuminate\Support\Facades\URL::forceScheme('https');
+       \Illuminate\Support\Facades\URL::forceScheme('https');
 
     }
 }
